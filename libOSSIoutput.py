30517ad4f8c9836177f105c36624956f9c97a398
Cody Johnson <codyjohnson144@gmail.com>
2017-07-19 12:12:10 -0500

    nothing special

--- libOSSIoutput.py
+++ libOSSIoutput.py
@@ -69,29 +69,43 @@ def format_raw(f_num, data_dir='.'):
     return formatted_data
 
 
-def simple_waves(formatted_data):
+def simple_waves(formatted_data, zb=0.02):
     ''' Simple function to compute Hm0 (m) with 4*std of demeaned water level
-        Also, it will return the detrended water level
+        Also, it will return the detrended water level and the depth over the
+        burst.
 
-        returns (eta, Hm0)
+        inputs are:
+            formatted_data = returned dictionary from format_raw()
+            zb = height of wave staff's tip from bottom (m). The default value
+                 is that used in the deltaSEES deployment as suggested by Dr.
+                 Chen.
+
+        returns (eta, Hm0, h)
     '''
 
-    # create series of Hm0 representative for each burst
-    Hm0 = pd.Series()
+    # create series of Hm0 and h representative for each burst
+    # Hm0 = pd.Series()
+    # h = pd.Series()
 
     # detrend and convert counts to meters (1000 mm = 4096 cts)
     data = {tstmp: data / 4096 for tstmp, data in formatted_data.items()}
     eta = {tstmp: out - out.mean() for tstmp, out in data.items()}
 
-    # compute signficant wave height
+    # compute zero-moment wave height and depth
+    h_dum = [obs.mean() + zb for obs in data.values()]
     Hm0_dum = [4 * obs.std() for obs in eta.values()]
 
     # Save to the pd.Series and reorder the timestamps
-    tstmp = [pd.to_datetime(key) for key in eta.keys()]
-    Hm0 = pd.Series(Hm0_dum, index=tstmp)
+    tstmp_data = [pd.to_datetime(key) for key in data.keys()]
+    tstmp_eta = [pd.to_datetime(key) for key in eta.keys()]
+
+    h = pd.Series(h_dum, index=tstmp_data)
+    h = h.sort_index()
+
+    Hm0 = pd.Series(Hm0_dum, index=tstmp_eta)
     Hm0 = Hm0.sort_index()
 
-    return (eta, Hm0)
+    return (eta, Hm0, h)
 
 
 def make_wf_mat(wl, fs=10, Tb=1200):
@@ -115,3 +129,19 @@ def make_wf_mat(wl, fs=10, Tb=1200):
         wf_mat[tstmp] = mat
 
     return wf_mat
+
+
+def compute_spectral(eta_mat, hmin=0.04):
+    ''' This function takes a wafo wave data matrix (returned by make_wf_mat)
+        and computes the spectral parameters of each burst which satisfies a
+        minimum depth requirement
+
+        input:
+            eta_mat = dictionary of wafo wave mats with the burst time stamp as
+                      keys
+            hmin = minimum depth (m) to compute the spectral params
+
+        output:
+    '''
+
+def compute_depth(eta_mat, zb=0.02)
